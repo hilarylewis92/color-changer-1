d109a85c03f906dbb20287a95d65a283da9f65cc
Hilary Lewis <hilarylewis92@gmail.com>
2016-09-06 14:02:44 -0600

    working script of project

--- scripts.js
+++ scripts.js
@@ -1,22 +1,20 @@
-// function getRandomColor() {
+// $('.btn').on('click', function() {
+//   function randomColorGenerator() {
 //     var letters = '0123456789ABCDEF';
 //     var color = '#';
-//     for (var i = 0; i < 6; i++ ) {
-//         color += letters[Math.floor(Math.random() * 16)];
-//     document.getElementById('body').style.background = color;
+//     for (var i = 0; i < 6; i++) {
+//       color += letters[Math.floor(Math.random() * 16)];
 //     }
-//     // return color;
-// }
+//   document.body.style.background = color;
+//   }
+// });
 
-var colorList = new Array ('#000000', '#ff0000', '#1075bc', '#07adeb', '#acdfe8', '#ff4265', '#54ff9f');
-var color = colorList[Math.floor(Math.random()*colorList.length)];
-
-// function randomColorGenerator() {
-//
-// }
+var colorList = new Array("#000000", "#ff0000", "#07adeb", "#ff4265", "#54ff9f", "#845656", "#ecda3a", "#381938");
 
 $('.btn').on('click', function() {
-  // for (var i = 0; i < 7; i++);
-  // color += colorList[Math.floor(Math.random()*colorList.length)];
-  document.body.style.background = color;
+  document.body.style.background = colorList[Math.floor(Math.random() * colorList.length)];
+});
+
+$('.btn-responsive').on('click', function() {
+  document.body.style.background = 'white';
 });
