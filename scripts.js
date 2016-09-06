0230b63ca59a831e7e84349c19abdd724fd34b72
Hilary Lewis <hilarylewis92@gmail.com>
2016-09-06 16:10:39 -0600

    refactor script for all colors

--- scripts.js
+++ scripts.js
@@ -1,20 +1,24 @@
-// $('.btn').on('click', function() {
-//   function randomColorGenerator() {
-//     var letters = '0123456789ABCDEF';
-//     var color = '#';
-//     for (var i = 0; i < 6; i++) {
-//       color += letters[Math.floor(Math.random() * 16)];
-//     }
-//   document.body.style.background = color;
-//   }
-// });
-
-var colorList = new Array("#000000", "#ff0000", "#07adeb", "#ff4265", "#54ff9f", "#845656", "#ecda3a", "#381938");
+function randomColorGenerator() {
+  return Math.floor(Math.random() * 255) + 1;
+}
 
 $('.btn').on('click', function() {
-  document.body.style.background = colorList[Math.floor(Math.random() * colorList.length)];
+    var color = randomColorGenerator();
+    $(document.body).css("background", "rgb(" + color + "," + color + "," + color + ")");
 });
 
 $('.btn-responsive').on('click', function() {
   document.body.style.background = 'white';
 });
+
+
+
+// var colorList = new Array("#000000", "#ff0000", "#07adeb", "#ff4265", "#54ff9f", "#845656", "#ecda3a", "#381938");
+//
+// $('.btn').on('click', function() {
+//   document.body.style.background = colorList[Math.floor(Math.random() * colorList.length)];
+// });
+//
+// $('.btn-responsive').on('click', function() {
+//   document.body.style.background = 'white';
+// });
